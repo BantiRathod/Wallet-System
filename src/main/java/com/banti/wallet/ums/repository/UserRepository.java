8b0ff4134ed430dc9b162d55bd215244d8c7c87e
bantiRathod <rathorb87@gmail.com>
2021-01-28 16:09:23 +0530

    changed userId type to long

--- src/main/java/com/banti/wallet/ums/repository/UserRepository.java
+++ src/main/java/com/banti/wallet/ums/repository/UserRepository.java
@@ -1,9 +1,8 @@
 package com.banti.wallet.ums.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.banti.wallet.ums.model.User;
 
-public interface UserRepository extends JpaRepository<User,Integer>
-{ 
+public interface UserRepository extends JpaRepository<User,Long>
+{
 }

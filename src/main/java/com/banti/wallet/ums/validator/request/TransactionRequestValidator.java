9dc7e4677f9778dcc9cd502df6c21674a3877cdf
bantiRathod <rathorb87@gmail.com>
2021-02-02 02:16:45 +0530

    added methods of request validation for all p2p,addMoney

--- src/main/java/com/banti/wallet/ums/validator/request/TransactionRequestValidator.java
+++ src/main/java/com/banti/wallet/ums/validator/request/TransactionRequestValidator.java
@@ -4,6 +4,7 @@ import com.banti.wallet.ums.controller.TransactionRequest;
 
 public class TransactionRequestValidator {
 	
+	//check request parameter for p2m api
 	public static void  p2mRequestValidator(TransactionRequest request) throws Exception {
 		if(request.getAmount()<=0) {
 			throw new Exception("Invaid Request Parameter, amount is negative or zero "+request.getAmount());
@@ -13,4 +14,22 @@ public class TransactionRequestValidator {
 		}
 	}
 
+	//check request parameter for p2p api
+	public static void  p2pRequestValidator(TransactionRequest request) throws Exception {
+		if(request.getAmount()<=0) {
+			throw new Exception("Invaid Request Parameter, amount is negative or zero "+request.getAmount());
+		}
+		if(request.getPayerMobileNo().equals(request.getPayeeMobileNo())) {
+			throw new Exception("Invaid Request Parameter, payer and payee is same");
+		}	
+	}
+	
+	
+	//check request parameter for addMoney API
+	public static void  addMoneyRequestValidator(TransactionRequest request) throws Exception {
+		if(request.getAmount()<=0) {
+			throw new Exception("Invaid Request Parameter, amount is negative or zero "+request.getAmount());
+		}
+		
+	}
 }

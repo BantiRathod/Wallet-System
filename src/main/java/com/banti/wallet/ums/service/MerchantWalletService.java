c5f3d95b44a257df84d6adcf1572cbbf4964942d
bantiRathod <rathorb87@gmail.com>
2021-02-03 22:08:00 +0530

    MerchantWalletService class started implementing MoneyMoventService Interface changed return type of update method added debitMoney and creditMoney method for money updation in wallet

--- src/main/java/com/banti/wallet/ums/service/MerchantWalletService.java
+++ src/main/java/com/banti/wallet/ums/service/MerchantWalletService.java
@@ -1,17 +1,22 @@
 package com.banti.wallet.ums.service;
 
 import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.banti.wallet.ums.model.BaseWallet;
 import com.banti.wallet.ums.model.MerchantWallet;
 import com.banti.wallet.ums.repository.MerchantWalletRepository;
 
-@Service 
+@Service (value="merchantWalletService")
 @Transactional
-public class MerchantWalletService 
+public class MerchantWalletService implements MoneyMovementService
 {
-
+	Logger logger=LoggerFactory.getLogger(MerchantWalletService.class);
 	@Autowired
 	 private MerchantWalletRepository merchantWalletRepository;
 	
@@ -30,9 +35,29 @@ public class MerchantWalletService
 		 merchantWalletRepository.save(merchantwallet);
 	 }
 
-	public void update(MerchantWallet payeeMerchantWallet) {
-		merchantWalletRepository.save(payeeMerchantWallet);
+	public MerchantWallet update(MerchantWallet payeeMerchantWallet) {
+		return merchantWalletRepository.save(payeeMerchantWallet);
 		
+	}
+
+	@Override
+	public BaseWallet debitMoney(BaseWallet wallet, double amount) {
+		MerchantWallet merchantWallet=(MerchantWallet) wallet;
+		logger.info("received debit request of amount {} from merchant {}",amount,merchantWallet);
+		merchantWallet.setBalance(merchantWallet.getBalance()-amount);
+		MerchantWallet updatedMerchantWallet = update(merchantWallet);
+		logger.info("merchant balance is {} after deducting amount{}",updatedMerchantWallet.getBalance(),amount);
+		return updatedMerchantWallet;
+	}
+
+	@Override
+	public BaseWallet creditMoney(BaseWallet wallet, double amount) {
+		MerchantWallet merchantWallet=(MerchantWallet) wallet;
+		logger.info("received credit request of amount {} from merchant {}",amount,merchantWallet);
+		merchantWallet.setBalance(merchantWallet.getBalance()+amount);
+		MerchantWallet updatedMerchantWallet = update(merchantWallet);
+		logger.info("merchant balance is {} after crediting amount{}",updatedMerchantWallet.getBalance(),amount);
+		return updatedMerchantWallet;
 	}	
 
 }

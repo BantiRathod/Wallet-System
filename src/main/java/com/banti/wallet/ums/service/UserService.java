9f1481aec95a4960ce798b5ed8e266d00d4d7e3f
bantiRathod <rathorb87@gmail.com>
2021-02-04 14:36:30 +0530

    moved code controller layer to service layer

--- src/main/java/com/banti/wallet/ums/service/UserService.java
+++ src/main/java/com/banti/wallet/ums/service/UserService.java
@@ -1,11 +1,17 @@
 package com.banti.wallet.ums.service;
 
+import java.util.Date;
 import java.util.List;
+import java.util.NoSuchElementException;
+
 import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.banti.wallet.ums.enums.PersonStatus;
 import com.banti.wallet.ums.model.User;
 import com.banti.wallet.ums.repository.UserRepository;
+import com.banti.wallet.ums.requestEntities.UserRequestEntity;
 
 @Service
 @Transactional
@@ -22,8 +28,28 @@ public class UserService {
         return userRepo.findAll();
     }
      
-    public void save(User user) {
-        userRepo.save(user);
+    public void updateUser(User user) throws NoSuchElementException
+    {
+    	 User existUser =  userRepo.findById(user.getUserId()).get();
+    	 
+         existUser.setUserName(user.getUserName());                         
+         existUser.setFirstName(user.getFirstName());                       
+         existUser.setLastName(user.getLastName());                          
+         existUser.setAddress(user.getAddress());
+         existUser.setMobileNo(user.getMobileNo());
+         userRepo.save(existUser);
+    }
+    public User saveUser(UserRequestEntity user) {
+    	User realUser=new User();
+    	realUser.setUserName(user.getUserName());
+    	realUser.setAddress(user.getAddress());
+    	realUser.setFirstName(user.getFirstName());
+    	realUser.setLastName(user.getLastName());
+    	realUser.setMobileNo(user.getMobileNo());
+    	realUser.setEmail(user.getEmail());
+    	realUser.setRegisterDate(new Date());
+    	realUser.setStatus(PersonStatus.ACTIVE.name());
+        return userRepo.save(realUser);
     }
      
     public User get(Long id) {

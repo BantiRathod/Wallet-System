2091cd42230a7ed85833d2cf61096c9f2d61e7e5
bantiRathod <rathorb87@gmail.com>
2021-02-05 14:43:07 +0530

    renamed classes name of UserRequestEntity to PersonRequestEntity and UserService to PersonService

--- src/main/java/com/banti/wallet/ums/controller/PersonController.java
+++ src/main/java/com/banti/wallet/ums/controller/PersonController.java
@@ -6,72 +6,81 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import com.banti.wallet.ums.model.User;
-import com.banti.wallet.ums.requestEntities.UserRequestEntity;
-import com.banti.wallet.ums.service.UserService;
+import com.banti.wallet.ums.model.Person;
+import com.banti.wallet.ums.requestEntities.PersonRequestEntity;
+import com.banti.wallet.ums.service.PersonService;
+
 import java.util.List;
 import java.util.NoSuchElementException;
 
 
 
 @RestController                                                                    //work at server side and remove view part
-public class UserController 
+public class PersonController 
 {
-	Logger logger=LoggerFactory.getLogger(UserController.class);
-	 @Autowired                                                                     //inject object of a class annotations    
-	private UserService userService;                                                //injecting variable of service class(at service layer)
-	 
+	Logger logger=LoggerFactory.getLogger(PersonController.class);
+	@Autowired                                                                     
+	private PersonService personService;                                             
+
      //RESTful API for getting all users
-	 @GetMapping("/users")
-	 public List<User> list()
+	 @GetMapping("/listOfPerson")
+	 public List<Person> list()
 	 {
-	    return userService.listAll();
+	    return personService.listAll();
 	 }
 	 
 	//RESTful API for getting the record particular user
-	 @GetMapping("/user/{id}")
-	 public ResponseEntity<User> get(@PathVariable Long id) {
+	 @GetMapping("/getPerson/{id}")
+	 public ResponseEntity<Person> get(@PathVariable Long id) {
 	     try {
-	         User user = userService.get(id);
-	         return new ResponseEntity<User> (user, HttpStatus.OK);
+	         Person person = personService.get(id);
+	         return new ResponseEntity<Person> (person, HttpStatus.OK);
 	     } catch (NoSuchElementException e) {
-	         return new ResponseEntity<User>(HttpStatus.NOT_FOUND);
+	         return new ResponseEntity<Person>(HttpStatus.NOT_FOUND);
 	     }      
 	 }
 	
 	 //RESTful API for Create data
-	 @PostMapping("/user")
-	 public User add(@RequestBody UserRequestEntity user) throws Exception {
+	 @PostMapping("/Sign-Up")
+	 public ResponseEntity<String> registerPerson(@RequestBody PersonRequestEntity user) {
 	     logger.info("request received to save User {}",user);
-	     return userService.saveUser(user);	   		 
+	     try
+	     {
+	    	 personService.saveUser(user);	 
+	         return new ResponseEntity<String>("person registered successfully And userName= "+user.getUserName()+", password= "+user.getPassword(),HttpStatus.OK);
+	     }
+	     catch(Exception e)
+	     {
+	        return new ResponseEntity<>(e.getMessage(),HttpStatus.CONFLICT);
+	     }
 	 }
 	 
 	 
 	// RESTful API for Update Operation
-	 @PutMapping("/user/{id}")
-	 public ResponseEntity<String> update(@RequestBody User user, @PathVariable Long id) {
+	 @PutMapping("/updatePerson/{id}")
+	 public ResponseEntity<String> update(@RequestBody Person user, @PathVariable Long id) {
 	     try
 	     {
-	         userService.updateUser(user);
-	         return new ResponseEntity<String>(" Record of the given id's user has been updated ",HttpStatus.OK);
+	    	 personService.updateUser(user);
+	         return new ResponseEntity<String>(" Record of the given id's person has been updated ",HttpStatus.OK);
 	     } catch (NoSuchElementException e) {
 	         return new ResponseEntity<String>(e.getMessage(), HttpStatus.NOT_FOUND);
 	     }      
 	 } 
 	
 	// RESTful API for Delete Operation
-	 @DeleteMapping("/user/{id}")
-	 public ResponseEntity<User> delete(@PathVariable Long id)
+	 @DeleteMapping("/deletePerson/{id}")
+	 public ResponseEntity<Person> delete(@PathVariable Long id)
 	 {
 		 try
 		 {
-		   User user= userService.get(id);
-		   userService.delete(id);
-		   return new ResponseEntity<User>(user,HttpStatus.OK);      
+		   Person user= personService.get(id);
+		   personService.delete(id);
+		   return new ResponseEntity<Person>(user,HttpStatus.OK);      
 	     }
 		  catch(NoSuchElementException e)
 		  {
-			 return new ResponseEntity<User>(HttpStatus.NOT_FOUND);
+			 return new ResponseEntity<Person>(HttpStatus.NOT_FOUND);
 		  }
 	 }
 }

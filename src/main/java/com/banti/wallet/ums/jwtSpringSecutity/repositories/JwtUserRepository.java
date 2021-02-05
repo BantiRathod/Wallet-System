1526fc25c873d3857af94e6887ba567b3e71cca2
bantiRathod <rathorb87@gmail.com>
2021-02-05 14:53:15 +0530

    class JwtUserDetailsService added to fatch userDetail from databases.

---
+++ src/main/java/com/banti/wallet/ums/jwtSpringSecutity/repositories/JwtUserRepository.java
@@ -0,0 +1,11 @@
+/*
+ * package com.banti.wallet.ums.jwtSpringSecutity.repositories;
+ * 
+ * import org.springframework.data.jpa.repository.JpaRepository;
+ * 
+ * import com.banti.wallet.ums.jwtSpringSecutity.models.JwtUser;
+ * 
+ * public interface JwtUserRepository extends JpaRepository<JwtUser,String> {
+ * 
+ * }
+ */
---
+++ src/main/java/com/banti/wallet/ums/jwtSpringSecutity/services/JwtUserDetailsService.java
@@ -0,0 +1,43 @@
+package com.banti.wallet.ums.jwtSpringSecutity.services;
+
+import java.util.ArrayList;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import com.banti.wallet.ums.model.Person;
+import com.banti.wallet.ums.service.PersonService;
+
+
+@Service
+public class JwtUserDetailsService implements UserDetailsService {
+	
+	Logger logger = LoggerFactory.getLogger(JwtUserDetailsService.class);
+	@Autowired 
+	private PersonService userService;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		
+	Person user = userService.findUserByUserName(username);
+		//String pass = bcryptEncoder.encode(user.getPassword());
+	
+	  logger.info("user received from database {}", user);
+		
+		if (user.getUserName().equals(username)) {
+		
+			return new  User(user.getUserName(), user.getPassword(),new ArrayList<>());
+		} else {
+			throw new UsernameNotFoundException("User not found with username: " + username);
+		}
+	}
+}
+	
+	
+

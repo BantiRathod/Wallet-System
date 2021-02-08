895130033a76b77b9e7f50078af01e5d428f6350
bantiRathod <rathorb87@gmail.com>
2021-02-09 00:21:10 +0530

    added class MerchantWalletRequest for containing data passed by client along with sending request.

---
+++ src/main/java/com/banti/wallet/ums/requestEntities/MerchantWalletRequest.java
@@ -0,0 +1,25 @@
+package com.banti.wallet.ums.requestEntities;
+
+public class MerchantWalletRequest {
+	private String mobileNo;
+	private Double balance;
+	public String getMobileNo() {
+		return mobileNo;
+	}
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+	public Double getBalance() {
+		return balance;
+	}
+	public void setBalance(Double balance) {
+		this.balance = balance;
+	}
+	@Override
+	public String toString() {
+		return "MerchantWalletRequest [mobileNo=" + mobileNo + ", balance=" + balance + "]";
+	}
+	
+	
+
+}

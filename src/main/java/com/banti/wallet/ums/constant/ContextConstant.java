883e6a951f7f9d18235d01f8e03a11baf3036d68
bantiRathod <rathorb87@gmail.com>
2021-02-09 03:53:04 +0530

    changed user_wallet to person_wallet

--- src/main/java/com/banti/wallet/ums/constant/ContextConstant.java
+++ src/main/java/com/banti/wallet/ums/constant/ContextConstant.java
@@ -2,8 +2,8 @@ package com.banti.wallet.ums.constant;
 
 public interface ContextConstant {
 	static String MERCHANT_WALLET="merchantWallet";
-	static String USER_WALLET="userWallet";
+	static String PERSON_WALLET="personWallet";
 	static String MERCHANT_ACCOUNT="merchantAccount";
-	static String USER_ACCOUNT="userAccount";
+	static String PERSON_ACCOUNT="personAccount";
 	static String BANK="bank";
 }

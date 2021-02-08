e98d826c74d5e4d39985f425eb48c57bf74f6f6a
bantiRathod <rathorb87@gmail.com>
2021-02-09 04:03:53 +0530

    UpdateMerchantWalletRequest new requst entity class

---
+++ src/main/java/com/banti/wallet/ums/requestEntities/UpdateMerchantWalletRequest.java
@@ -0,0 +1,28 @@
+package com.banti.wallet.ums.requestEntities;
+
+public class UpdateMerchantWalletRequest {
+
+	    private String mobileNo;
+	    private Double balance;
+	    
+		public String getMobileNo() {
+			return mobileNo;
+		}
+		public void setMobileNo(String mobileNo) {
+			this.mobileNo = mobileNo;
+		}
+		public Double getBalance() {
+			return balance;
+		}
+		public void setBalance(Double balance) {
+			this.balance = balance;
+		}
+		
+		@Override
+		public String toString() {
+			return "UpdateMerchantWalletRequest [mobileNo=" + mobileNo + ", balance=" + balance + "]";
+		}
+	    
+	    
+	
+}

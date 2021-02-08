8bda68c0eb684c89c152b03bb23191519e39fcbe
bantiRathod <rathorb87@gmail.com>
2021-02-09 03:56:31 +0530

    1.added toUpdateMerchantallet to update merchant wallet 2.MerchantWalletRequest added to take referance of object sendig by user 3.tooked data from elasticsearch instead of mysql

--- src/main/java/com/banti/wallet/ums/controller/MerchantWalletController.java
+++ src/main/java/com/banti/wallet/ums/controller/MerchantWalletController.java
@@ -1,9 +1,5 @@
 package com.banti.wallet.ums.controller;
 
-import java.util.Date;
-import java.util.List;
-import java.util.NoSuchElementException;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,9 +8,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import com.banti.wallet.ums.model.MerchantWallet;
+
+import com.banti.wallet.ums.elasticsearch.models.ElasticMerchantWallet;
+import com.banti.wallet.ums.requestEntities.MerchantWalletRequest;
+import com.banti.wallet.ums.requestEntities.UpdateMerchantWalletRequest;
 import com.banti.wallet.ums.service.MerchantWalletService;
 
 @RestController
@@ -26,40 +26,62 @@ public class MerchantWalletController
 	private MerchantWalletService merchantWalletService;
 	
 	@GetMapping("/merchantWallets")
-	public ResponseEntity<List<MerchantWallet>> listAll()
+	public ResponseEntity<Iterable<ElasticMerchantWallet>> toGetListOfAllMerchantWallets()
 	{
 		try
 		{
-	         List<MerchantWallet> list= merchantWalletService.getAll();
-	         return new ResponseEntity<List<MerchantWallet>>(list,HttpStatus.OK);
+	         Iterable<ElasticMerchantWallet> list= merchantWalletService.getListOfAllMerchantWallet();
+	         return new ResponseEntity<Iterable<ElasticMerchantWallet>>(list,HttpStatus.OK);
 		}
-		catch(NoSuchElementException e)
+		catch(Exception e)
 		{
-			return new ResponseEntity<List<MerchantWallet>>(HttpStatus.NOT_FOUND);
+			logger.error(e.getMessage());
+			return new ResponseEntity<Iterable<ElasticMerchantWallet>>(HttpStatus.NOT_FOUND);
 		}	
 	}
 	
 	@GetMapping("/merchantWallet/{mobileNo}")
-	 public ResponseEntity<MerchantWallet> fatchWallet(@PathVariable String mobileNo)
+	 public ResponseEntity<ElasticMerchantWallet> toGetMerchantWallet(@PathVariable String mobileNo)
 	 {
 		try
 		{
-			MerchantWallet existWallet = merchantWalletService.get(mobileNo);
-	         return new ResponseEntity<MerchantWallet>(existWallet,HttpStatus.OK);
+			 ElasticMerchantWallet existWallet = merchantWalletService.getMerchantWallet(mobileNo);
+	         return new ResponseEntity<ElasticMerchantWallet>(existWallet,HttpStatus.OK);
 		}
-		catch(NoSuchElementException e)
+		catch(Exception e)
 		{
-			return new ResponseEntity<MerchantWallet>(HttpStatus.NOT_FOUND);
+			logger.error(e.getMessage());
+			return new ResponseEntity<ElasticMerchantWallet>(HttpStatus.NOT_FOUND);
 		}	
 	 }
 	 
 	@PostMapping("/merchantWallet")
-	public String createWallet(@RequestBody MerchantWallet merchantWallet)
+	public String createWallet(@RequestBody MerchantWalletRequest merchantWallet)
       {
 		logger.info("merchantWallet received {} ", merchantWallet);
-		merchantWallet.setMerchantWalletcreatedDate(new Date());
-		merchantWalletService.create(merchantWallet);
-		return "New merchant wallet of "+merchantWallet.getMobileNo()+"has heen created on "+ merchantWallet.getMerchantWalletcreatedDate();
-      }
+		try
+		{
+		merchantWalletService.createMerchantWallet(merchantWallet);
+		return "New merchant wallet of "+merchantWallet.getMobileNo()+" has heen created";
+        }catch(Exception e)
+		{
+        	return "Exception occured, "+e.getMessage();
+		}
+	 }
+	
+	@PutMapping("/merchantWallet/{mobileNo}")
+	public String toUpdateMerchantallet(@PathVariable String mobileNo, @RequestBody UpdateMerchantWalletRequest updateMerchantWalletRequest)
+	{
+		logger.info("UpdateMerchantWalletRequest received {} ",updateMerchantWalletRequest);
+		try
+		{
+			merchantWalletService.updateMerchantWallet(updateMerchantWalletRequest, mobileNo);
+			return "Merchant wallet has been updated successful ";
+		}catch(Exception e)
+		{
+			return "Exception occured, "+e.getMessage();
+		}	
+    }
+	
 	
 }

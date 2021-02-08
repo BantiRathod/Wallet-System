e2b3f2644ffeb23b8bf3ebde454cfd5312f1fe21
bantiRathod <rathorb87@gmail.com>
2021-02-09 00:21:57 +0530

    added class UpdatePersonRequest for containing data passed by client along with sending request.

---
+++ src/main/java/com/banti/wallet/ums/requestEntities/UpdatePersonRequest.java
@@ -0,0 +1,63 @@
+package com.banti.wallet.ums.requestEntities;
+
+
+public class UpdatePersonRequest {
+
+	private String userName;
+	private String firstName;
+	private String lastName;
+	private String address;
+	private String email;
+    private String mobileNo; 
+	private String password;
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getMobileNo() {
+		return mobileNo;
+	}
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	@Override
+	public String toString() {
+		return "UpdatePersonRequest [userName=" + userName + ", firstName=" + firstName + ", lastName=" + lastName
+				+ ", address=" + address + ", email=" + email + ", mobileNo=" + mobileNo + ", password=" + password
+				+ "]";
+	}
+	
+}

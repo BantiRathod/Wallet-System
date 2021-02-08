301e21d8af5e8082733aa1cf33d9d04c78049a71
bantiRathod <rathorb87@gmail.com>
2021-02-09 03:57:47 +0530

    findAllByPayerNo method was added

--- src/main/java/com/banti/wallet/ums/elasticsearch/repositories/ElasticWalletTransactionRepository.java
+++ src/main/java/com/banti/wallet/ums/elasticsearch/repositories/ElasticWalletTransactionRepository.java
@@ -4,4 +4,8 @@ import org.springframework.data.elasticsearch.repository.ElasticsearchRepository
 import com.banti.wallet.ums.elasticsearch.models.ElasticWalletTransaction;
 
 public interface ElasticWalletTransactionRepository extends ElasticsearchRepository<ElasticWalletTransaction, Long> {
+
+	public Iterable<ElasticWalletTransaction> findAllByPayerNo(String mobileNo);
+		
+	
 }

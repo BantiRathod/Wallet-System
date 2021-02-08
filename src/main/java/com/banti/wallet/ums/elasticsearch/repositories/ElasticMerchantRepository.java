b8ce4d3cea440021c582e6523d3b29a24b46f6bb
bantiRathod <rathorb87@gmail.com>
2021-02-08 16:37:45 +0530

    changed field id type string to long

--- src/main/java/com/banti/wallet/ums/elasticsearch/repositories/ElasticMerchantRepository.java
+++ src/main/java/com/banti/wallet/ums/elasticsearch/repositories/ElasticMerchantRepository.java
@@ -3,6 +3,8 @@ package com.banti.wallet.ums.elasticsearch.repositories;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import com.banti.wallet.ums.elasticsearch.models.ElasticMerchant;
 
-public interface ElasticMerchantRepository extends ElasticsearchRepository<ElasticMerchant ,String> {
+public interface ElasticMerchantRepository extends ElasticsearchRepository<ElasticMerchant , Long> {
+
+	ElasticMerchant findByMobileNo(String payeeMobileNo);
 
 }

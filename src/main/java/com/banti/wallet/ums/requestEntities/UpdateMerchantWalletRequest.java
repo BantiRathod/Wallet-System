f587fa1769a147fcde97f847ec892a67a482fb17
bantiRathod <rathorb87@gmail.com>
2021-02-11 02:42:39 +0530

    removed balance field because it is not updatable.

--- src/main/java/com/banti/wallet/ums/requestEntities/UpdateMerchantWalletRequest.java
+++ src/main/java/com/banti/wallet/ums/requestEntities/UpdateMerchantWalletRequest.java
@@ -3,7 +3,7 @@ package com.banti.wallet.ums.requestEntities;
 public class UpdateMerchantWalletRequest {
 
 	    private String mobileNo;
-	    private Double balance;
+	    //private Double balance;
 	    
 		public String getMobileNo() {
 			return mobileNo;
@@ -11,16 +11,14 @@ public class UpdateMerchantWalletRequest {
 		public void setMobileNo(String mobileNo) {
 			this.mobileNo = mobileNo;
 		}
-		public Double getBalance() {
-			return balance;
-		}
-		public void setBalance(Double balance) {
-			this.balance = balance;
-		}
+		/*
+		 * public Double getBalance() { return balance; } public void setBalance(Double
+		 * balance) { this.balance = balance; }
+		 */
 		
 		@Override
 		public String toString() {
-			return "UpdateMerchantWalletRequest [mobileNo=" + mobileNo + ", balance=" + balance + "]";
+			return "UpdateMerchantWalletRequest [mobileNo=" + mobileNo + "]";
 		}
 	    
 	    

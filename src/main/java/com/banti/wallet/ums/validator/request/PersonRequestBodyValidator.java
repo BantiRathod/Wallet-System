ce30003a0d7ac6201ca838f3e1fe2bc4d63e1419
bantiRathod <rathorb87@gmail.com>
2021-02-10 23:48:45 +0530

    PersonRequestBodyValidator class created in order to valid request body passed by client.

---
+++ src/main/java/com/banti/wallet/ums/validator/request/PersonRequestBodyValidator.java
@@ -0,0 +1,26 @@
+package com.banti.wallet.ums.validator.request;
+
+import org.springframework.stereotype.Service;
+
+import com.banti.wallet.ums.requestEntities.PersonRequestEntity;
+import com.banti.wallet.ums.requestEntities.UpdatePersonRequest;
+
+@Service
+public class PersonRequestBodyValidator {
+
+	public void personRequestIdValidation(Long id) throws Exception
+	{
+		if(id<0)
+			throw new Exception("Invalid Id passed , "+ id);
+	}
+
+    //WHEN CLIENT MAKES REQUEST FOR PERSON DETAILS UPDATION 
+	public void personRequestBodyAndIdValidation(UpdatePersonRequest person, Long id) throws Exception{
+		if(id<0)
+			throw new Exception("Invalid Id passed , "+ id);
+		
+		if(person.getMobileNo().length()!=10)
+			throw new Exception("Invalid MobileNo found , "+ person.getMobileNo());
+		
+	}
+}

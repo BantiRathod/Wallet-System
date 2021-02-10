5f7f45e83e523a87beb8d40c67d5af3eec74cc16
bantiRathod <rathorb87@gmail.com>
2021-02-11 01:03:07 +0530

    1. Applied request body validation over delete, update, get, and create.

--- src/main/java/com/banti/wallet/ums/controller/MerchantController.java
+++ src/main/java/com/banti/wallet/ums/controller/MerchantController.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,72 +19,92 @@ import com.banti.wallet.ums.elasticsearch.models.ElasticMerchant;
 import com.banti.wallet.ums.requestEntities.MerchantRequest;
 import com.banti.wallet.ums.requestEntities.UpdateMerchantRequest;
 import com.banti.wallet.ums.service.MerchantService;
+import com.banti.wallet.ums.validator.request.MerchantRequestBodyValidator;
 
 
 @RestController
 public class MerchantController {
 
-	Logger logger=LoggerFactory.getLogger(MerchantController.class);
+Logger logger=LoggerFactory.getLogger(MerchantController.class);
 
 @Autowired
 private MerchantService merchantService;
 
+@Autowired
+private MerchantRequestBodyValidator merchantRequestBodyValidator;
 
 @GetMapping("/merchants")
 public ResponseEntity<Iterable<ElasticMerchant>> toGetlistOfAllMerchants()
 {
 	try
 	{
-       Iterable<ElasticMerchant> list=merchantService.getListOfAllMerchants();
+            Iterable<ElasticMerchant> list=merchantService.getListOfAllMerchants();
             return new ResponseEntity<Iterable<ElasticMerchant>>(list,HttpStatus.OK);
 	}
 	catch(NoSuchElementException e)
 	{
-		return new ResponseEntity<Iterable<ElasticMerchant>>(HttpStatus.NOT_FOUND);
+		  return new ResponseEntity<Iterable<ElasticMerchant>>(HttpStatus.NOT_FOUND);
 	}	
 }
 
 @GetMapping("/merchant/{id}")
- public ResponseEntity< ElasticMerchant > fatchWallet(@PathVariable Long id)
+ public ResponseEntity< ElasticMerchant > getMerchantUsingId(@PathVariable Long id)
  {
 	try
 	{
+		 merchantRequestBodyValidator.merchantRequestIdValidation(id);
          ElasticMerchant merchant = merchantService.getMerchant(id);
+         logger.info("responsed merchant record {} "+ merchant);
          return new ResponseEntity< ElasticMerchant >(merchant,HttpStatus.OK);
 	}
 	catch(Exception e)
 	{
-		logger.error(e.getMessage());
+		logger.error("Exception occured, "+e.getMessage());
 		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 	}	
  }
  
     @PostMapping("/merchant")
-    public String toMerchantRegistration(@RequestBody MerchantRequest merchant )
+    public ResponseEntity<String> toMerchantRegistration(@RequestBody MerchantRequest merchant )
   {
 	 logger.info("merchantRequest  recieved {} ", merchant);
 	 try
 	 {
-	 merchantService.createMerchantAccount(merchant);
-	 return " New merchant account has been created, shopName is "+merchant.getShopName();
+		 //TO VALITEDE THE REQUEST BODY
+		  merchantRequestBodyValidator.createMerchantValidation(merchant);
+	      merchantService.createMerchantAccount(merchant);
+	      return new ResponseEntity<String>(" New merchant account has been created, shopName is "+merchant.getShopName(),HttpStatus.OK);
 	 }catch(Exception e)
 	 {
-		return "Exception occured "+ e.getMessage();
+		return new ResponseEntity<String>("Exception occured "+ e.getMessage(),HttpStatus.OK);
 	 }
   }
 
 @PutMapping("/merchant/{id}")
-public String toUpdateMerchantAccount(@RequestBody 	UpdateMerchantRequest merchant ,@ PathVariable Long id)
+public ResponseEntity<String> toUpdateMerchantAccount(@RequestBody 	UpdateMerchantRequest merchant ,@ PathVariable Long id)
 {
 	try
 	{
 		merchantService.updateMerchantAccount(merchant, id);
-		return " Merchant record has been updated";
+		return new ResponseEntity<String>("Merchant record has been updated",HttpStatus.OK);
 	}catch(Exception e)
 	{
-		return "run time exception occured "+e.getMessage();
-	}
-	
+		return new ResponseEntity<String>("Exception occured "+ e.getMessage(),HttpStatus.OK);
+	}	
 }
 
+@DeleteMapping("/merchant/{id}")
+public ResponseEntity<String> toDeleteExistMerchantAccount(@PathVariable Long id)
+{
+	try
+	{
+		//TO REQUEST BODY VALIDATOR
+		 merchantRequestBodyValidator.merchantRequestIdValidation(id);
+		 merchantService.deleteMerchantAccount(id); 
+		 return new ResponseEntity<String>("Merchant account deleted successfully of id ="+id,HttpStatus.OK);
+	}catch(Exception e)
+	{
+	     return new ResponseEntity<String>("Exception occured, "+e.getMessage(),HttpStatus.OK);
+	}
+}
 }

4d73340603adab51ca00890b00ee7d998d91a181
bantiRathod <rathorb87@gmail.com>
2021-02-10 23:46:08 +0530

    Each index name should be in lower case, not in cammle case or capitalization.

--- src/main/java/com/banti/wallet/ums/elasticsearch/models/ElasticPersonWallet.java
+++ src/main/java/com/banti/wallet/ums/elasticsearch/models/ElasticPersonWallet.java
@@ -8,7 +8,7 @@ import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
 
-@Document(indexName="ElasticPersonWallet",shards=1)
+@Document(indexName="elastic_person_wallet",shards=1)
 public class ElasticPersonWallet {
 
     @Id

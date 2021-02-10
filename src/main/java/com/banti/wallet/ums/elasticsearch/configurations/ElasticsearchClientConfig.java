49fb2e19686d8b803e3559733ff0de5dcbab51bc
bantiRathod <rathorb87@gmail.com>
2021-02-10 23:44:19 +0530

    removed EnableElasticsearchRepositories this config from here and added in main application class

--- src/main/java/com/banti/wallet/ums/elasticsearch/configurations/ElasticsearchClientConfig.java
+++ src/main/java/com/banti/wallet/ums/elasticsearch/configurations/ElasticsearchClientConfig.java
@@ -2,12 +2,12 @@ package com.banti.wallet.ums.elasticsearch.configurations;
 
 import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.RestClients;
 import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
-import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
 
 @Configuration
 public class ElasticsearchClientConfig extends 

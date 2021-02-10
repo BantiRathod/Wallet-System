c99c55d260227eab0d7ae66126f48e5db1d9a769
bantiRathod <rathorb87@gmail.com>
2021-02-10 23:49:38 +0530

    personWalletMobileNoValidation method added to check mobile number

--- src/main/java/com/banti/wallet/ums/validator/request/PersonWalletRequestValidator.java
+++ src/main/java/com/banti/wallet/ums/validator/request/PersonWalletRequestValidator.java
@@ -3,17 +3,26 @@ package com.banti.wallet.ums.validator.request;
 import org.springframework.stereotype.Service;
 
 import com.banti.wallet.ums.requestEntities.PersonWalletRequest;
+import com.banti.wallet.ums.requestEntities.UpdatePersonWalletRequest;
 
 @Service
 public class PersonWalletRequestValidator 
 {
 
-	public void  personWalletRequestValidation(PersonWalletRequest personWalletRequest ) throws Exception
+	public void  personWalletRequestBodyValidation(PersonWalletRequest personWalletRequest ) throws Exception
 	{
 		if( personWalletRequest.getBalance() < 0)
 			 throw new Exception("invalid amount, "+personWalletRequest.getBalance()) ;
 		
 		if(personWalletRequest.getMobileNo().length()!=10)
 			 throw new Exception("invalid mobile number , "+personWalletRequest.getMobileNo()) ;
+	}
+
+	public void personWalletMobileNoValidation(UpdatePersonWalletRequest personWallet, String mobileNo) throws Exception {
+		   if((personWallet!=null) && personWallet.getMobileNo().length()!=10)
+	        	 throw new Exception("\"invalid mobile number ,"+ personWallet.getMobileNo());
+		   else if(mobileNo.length()!=10)
+			   throw new Exception("\"invalid mobile number ,"+ personWallet.getMobileNo());
+			      
 	}
 }

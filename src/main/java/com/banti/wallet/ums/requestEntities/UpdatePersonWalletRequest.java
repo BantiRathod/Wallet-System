d9062192818dc397e1d8b2b8d52842f453ab8c67
bantiRathod <rathorb87@gmail.com>
2021-02-10 23:46:57 +0530

    removed balance variable from class because balance can not be updated.

--- src/main/java/com/banti/wallet/ums/requestEntities/UpdatePersonWalletRequest.java
+++ src/main/java/com/banti/wallet/ums/requestEntities/UpdatePersonWalletRequest.java
@@ -3,7 +3,6 @@ package com.banti.wallet.ums.requestEntities;
 public class UpdatePersonWalletRequest {
 
 	private String mobileNo;
-	private Double balance;
 	
 	public String getMobileNo() {
 		return mobileNo;
@@ -11,15 +10,10 @@ public class UpdatePersonWalletRequest {
 	public void setMobileNo(String mobileNo) {
 		this.mobileNo = mobileNo;
 	}
-	public Double getBalance() {
-		return balance;
-	}
-	public void setBalance(Double balance) {
-		this.balance = balance;
-	}
+
 	@Override
 	public String toString() {
-		return "UpdatePersonWalletRequest [mobileNo=" + mobileNo + ", balance=" + balance + "]";
+		return "UpdatePersonWalletRequest [mobileNo=" + mobileNo + "]";
 	}
 	
 	

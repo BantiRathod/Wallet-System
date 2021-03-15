caf09ca33b6134ef8c0acee54005b82cb5ccbe85
bantiRathod <rathorb87@gmail.com>
2021-03-15 10:43:25 +0530

    created payout interface for payout table

---
+++ src/main/java/com/banti/wallet/ums/repository/PayoutRepository.java
@@ -0,0 +1,9 @@
+package com.banti.wallet.ums.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.banti.wallet.ums.scheduling.Payout;
+
+public interface PayoutRepository extends JpaRepository<Payout,Long>{
+   
+	 }

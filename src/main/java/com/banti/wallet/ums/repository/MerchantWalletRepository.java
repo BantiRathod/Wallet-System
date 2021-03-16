1336a536a6382dc4d10a3b3ca2773657009f212e
bantiRathod <rathorb87@gmail.com>
2021-03-17 00:19:53 +0530

    wrote custome jpql query to get enabled merchant wallet.

--- src/main/java/com/banti/wallet/ums/repository/MerchantWalletRepository.java
+++ src/main/java/com/banti/wallet/ums/repository/MerchantWalletRepository.java
@@ -1,11 +1,16 @@
 package com.banti.wallet.ums.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.banti.wallet.ums.model.MerchantWallet;
 
 
 public interface MerchantWalletRepository extends JpaRepository<MerchantWallet,String> {
+
+	@Query("select mw from MerchantWallet mw where mw.status= 'ENABLED' ")
+	List<MerchantWallet> getEnabledMerchantWalletList();
 
 }

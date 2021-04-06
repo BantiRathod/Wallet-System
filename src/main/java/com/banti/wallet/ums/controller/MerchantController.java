b5fe17e4885f8a7e7d4539b73e7925025868d4f2
bantiRathod <rathorb87@gmail.com>
2021-04-06 23:46:14 +0530

    ADDED COMMENTS FOR API.

--- src/main/java/com/banti/wallet/ums/controller/MerchantController.java
+++ src/main/java/com/banti/wallet/ums/controller/MerchantController.java
@@ -29,10 +29,16 @@ Logger logger=LoggerFactory.getLogger(MerchantController.class);
 
 @Autowired
 private MerchantService merchantService;
-
 @Autowired
 private MerchantRequestBodyValidator merchantRequestBodyValidator;
 
+/**
+     *  THIS API IS REPONSIBLE FOR RETREIVING LIST OF ALL REGISTERED MERCHANTS.
+	 * 
+	 * @return LIST OF MERCHZNTS WOULD BE RETURN IF EXIST, ELSE RETURN HTTPSTATUS.
+	 * OK        :- GOT MERCHANTS RECORDS.
+	 * NOT_FOUND :- DIDN'T GOT MERCHANTS RECORDS.
+ */
 @GetMapping("/merchants")
 public ResponseEntity<Iterable<ElasticMerchant>> toGetlistOfAllMerchants()
 {
@@ -49,11 +55,25 @@ public ResponseEntity<Iterable<ElasticMerchant>> toGetlistOfAllMerchants()
 	}	
 }
 
+
+/**
+ * THIS API IS REPONSIBLE FOR RETREIVING CEERTIAN MERCHANT'S RECORDS.
+ * 
+ * @param id IS PARAMETER PASSD BY USER WHILE MAKING HTTP REQUEST BY USER.
+ * 
+ * merchantRequestIdValidation METHOD WILL  VALIDET PASSED id, IF IT IS VALIDET AND THIS ID EXIST IN THE SYSTEM.. 
+ * THEN WE WILL CALL METHOSD OF SERVICE FOR FATCHING RECORD FROM DATA BASE OTHERWISE THROW A EXCEPTION .  
+ * 
+ * @return GOING TO RETURN A MERCHANT RECORD IF EXIST, ELSE RETURN HTTPSTATUS.
+ * OK        :- GOT MERCHANTS RECORDS.
+ * NOT_FOUND :- DIDN'T GOT MERCHANTS RECORDS.
+ */
 @GetMapping("/merchant/{id}")
  public ResponseEntity< ElasticMerchant > getMerchantUsingId(@PathVariable Long id)
  {
 	try
-	{
+	{   
+		 // INORDER TO VALIDET PASSED ID
 		 merchantRequestBodyValidator.merchantRequestIdValidation(id);
          ElasticMerchant merchant = merchantService.getMerchant(id);
          logger.info("responsed merchant record {} "+ merchant);
@@ -65,10 +85,20 @@ public ResponseEntity<Iterable<ElasticMerchant>> toGetlistOfAllMerchants()
 		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 	}	
  }
- 
+   
+/**
+ * THIS API IS REPONSIBLE FOR CREATING NEW MERCHANT ACCOUNT.
+ * 
+ * @param merchant TYPE OF MerchantRequest IS BODY PASSD BY USER WHILE MAKING HTTP REQUEST BY USER.
+ * 
+ * createMerchantValidation METHOD WILL  VALIDET PASSED merchant BODY, IF IT IS VALIDET AND THERE IS NEW MOBILE NO
+ * THEN WE WILL CALL METHOSD OF SERVICE FOR CREATING NEW MERCHANT ACCOUNT, OTHERWISE THROW A EXCEPTION.  
+ * 
+ * @return GOING TO RETURN A CONFIRMATION MESSAGE, ELSE RETURN HTTPSTATUS.
+ */
     @PostMapping("/merchant")
     public ResponseEntity<String> toMerchantRegistration(@RequestBody MerchantRequest merchant )
-  {
+   {
 	 logger.info("merchantRequest  recieved {} ", merchant);
 	 try
 	 {
@@ -82,11 +112,19 @@ public ResponseEntity<Iterable<ElasticMerchant>> toGetlistOfAllMerchants()
 	 }
   }
 
+    
+ /**
+  * THIS API IS REPONSIBLE FOR UPDATTING EXIST MERCHANT ACCOUNT.
+  * 
+  * @param merchant AND id ARE THE REQUESTBODY AND PARA. RESPECTIVELY BEING PASSED BY USER.
+  *   
+  * @return WILL GIVE CONFIRMATION MESSAGE OF UPDATTING OR EXCEPTION.
+  */
 @PutMapping("/merchant/{id}")
 public ResponseEntity<String> toUpdateMerchantAccount(@RequestBody 	UpdateMerchantRequest merchant ,@ PathVariable Long id)
 {
 	try
-	{
+	{   
 		merchantService.updateMerchantAccount(merchant, id);
 		return new ResponseEntity<String>("Merchant record has been updated",HttpStatus.OK);
 	}catch(Exception e)
@@ -94,6 +132,17 @@ public ResponseEntity<String> toUpdateMerchantAccount(@RequestBody 	UpdateMercha
 		return new ResponseEntity<String>("Exception occured "+ e.getMessage(),HttpStatus.OK);
 	}	
 }
+
+/**
+ * THIS API IS REPONSIBLE FOR DELETTING EXIST MERCHANT ACCOUNT.
+ * 
+ * @param id IS PARA BEING PASSED BY USER.
+ *   
+ *merchantRequestIdValidation METHOD WILL  VALIDET PASSED ID, IF IT IS VALIDET 
+ * THEN WE WILL CALL METHOSD OF SERVICE FOR UPDATTING EXIST MERCHANT ACCOUNT, OTHERWISE THROW A EXCEPTION. 
+ *  
+ * @return WILL GIVE CONFIRMATION MESSAGE OF UPDATTING OR EXCEPTION.
+ */
 
 @DeleteMapping("/merchant/{id}")
 public ResponseEntity<String> toDeleteExistMerchantAccount(@PathVariable Long id)

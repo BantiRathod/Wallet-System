2561ff3b527333baea421553208e5666039d1d20
bantiRathod <rathorb87@gmail.com>
2021-06-05 16:15:23 +0530

    removed mobile no. validation part

--- src/main/java/com/banti/wallet/ums/validator/request/PersonRequestBodyValidator.java
+++ src/main/java/com/banti/wallet/ums/validator/request/PersonRequestBodyValidator.java
@@ -3,7 +3,7 @@ package com.banti.wallet.ums.validator.request;
 import org.springframework.stereotype.Service;
 
 import com.banti.wallet.ums.requestEntities.PersonRequestEntity;
-import com.banti.wallet.ums.requestEntities.UpdatePersonRequest;
+//import com.banti.wallet.ums.requestEntities.UpdatePersonRequest;
 
 @Service
 public class PersonRequestBodyValidator {
@@ -15,12 +15,9 @@ public class PersonRequestBodyValidator {
 	}
 
     //WHEN CLIENT MAKES REQUEST FOR PERSON DETAILS UPDATION 
-	public void personRequestBodyAndIdValidation(UpdatePersonRequest person, Long id) throws Exception{
+	public void personRequestBodyAndIdValidation(Long id) throws Exception{
 		if(id<0)
 			throw new Exception("Invalid Id passed , "+ id);
-		
-		if(person.getMobileNo().length()!=10)
-			throw new Exception("Invalid MobileNo found , "+ person.getMobileNo());
 		
 	}
 

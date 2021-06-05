0e68b9e130845744a2aed299130492cfce33f1b4
bantiRathod <rathorb87@gmail.com>
2021-06-05 16:05:26 +0530

    mobile no and email id can not br updated that's why removed them

--- src/main/java/com/banti/wallet/ums/requestEntities/UpdateMerchantRequest.java
+++ src/main/java/com/banti/wallet/ums/requestEntities/UpdateMerchantRequest.java
@@ -5,9 +5,6 @@ public class UpdateMerchantRequest {
 	
 	private String shopName;
 	private String address;
-	private String email;
-	private String mobileNo;
-	private String password;
 	
 	public String getShopName() {
 		return shopName;
@@ -21,29 +18,11 @@ public class UpdateMerchantRequest {
 	public void setAddress(String address) {
 		this.address = address;
 	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getMobileNo() {
-		return mobileNo;
-	}
-	public void setMobileNo(String mobileNo) {
-		this.mobileNo = mobileNo;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
+
 	
 	@Override
 	public String toString() {
-		return "UpdateMerchantRequest [shopName=" + shopName + ", address=" + address + ", email=" + email
-				+ ", mobileNo=" + mobileNo + ", password=" + password + "]";
+		return "UpdateMerchantRequest [shopName=" + shopName + ", address=" + address + "+ ]";
 	}
 	
 	
